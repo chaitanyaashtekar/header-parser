902c2f485dadfab640ea5c00ce3f3ad7d732605a
chaitanyaashtekar <chaiashtekar@gmail.com>
2016-09-20 21:34:23 +0530

    ip add corection

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ app.set('port', (process.env.PORT || 5000));
 
 app.get('/',function(req,res){
 	req.setEncoding('utf8');
-	
+	ipaddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+console.log(ipaddress);
 	var data={
-		ipaddress:req.headers.host,
+		ipaddress:ipaddress,
 		language:req.headers['accept-language'].split(',')[0],
 		software:req.headers['user-agent'].split(/\s/).slice(1,4).join('')
 	}
